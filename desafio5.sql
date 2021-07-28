78cdcba0964759ee3428fc9a5000241fdff98210
Gildeir <gildeirlopes@gmail.com>
2021-07-28 03:05:09 -0300

    adicionando requisitos 4 e 5

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+SELECT t1.JOB_TITLE AS Cargo,
+(MAX_SALARY - MIN_SALARY) 'Varicação Salarial',
+ROUND(MIN_SALARY / 12, 2) 'Média mínima mensal ',
+ROUND(MAX_SALARY / 12, 2) 'Média máxima mensal'
+FROM hr.jobs AS t1
+INNER JOIN hr.employees AS t2
+ON t1.JOB_ID = t2.JOB_ID
+GROUP BY Cargo
+ORDER BY (MAX_SALARY - MIN_SALARY) ASC;
+
