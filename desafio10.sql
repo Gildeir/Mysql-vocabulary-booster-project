528f24e26d124effd85edfe018fcd09718bd9d77
Gildeir <gildeirlopes@gmail.com>
2021-07-28 19:39:33 -0300

    req 10 code climate

--- desafio10.sql
+++ desafio10.sql
@@ -8,4 +8,4 @@ INNER JOIN w3schools.order_details AS orderDetail
 ON product.ProductID = orderDetail.ProductID
 GROUP BY `Produto`
 HAVING `Média` > 20
-ORDER BY `Média` ASC, `Produto` ASC
+ORDER BY `Média` ASC, `Produto` ASC;
