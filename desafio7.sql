7b27cbbccafbea189f0e881ca68778c609b1ca08
Gildeir <gildeirlopes@gmail.com>
2021-07-28 20:28:37 -0300

    req 6,7

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT
-CONCAT(UPPER(t1.first_name), ' ',UPPER(t1.last_name)) AS `Nome Completo`,
+CONCAT(UPPER(t1.first_name), ' ',UPPER(t1.last_name)) AS `Nome completo`,
 t2.START_DATE AS `Data de início`,
 t1.salary AS `Salário`
 FROM hr.employees AS t1
@@ -7,4 +7,4 @@ INNER JOIN
 hr.job_history AS t2
 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
 WHERE MONTH(t2.START_DATE) BETWEEN 1 AND 3
-ORDER BY `Nome Completo` ASC, `Data de início` ASC;
+ORDER BY `Nome completo` ASC, `Data de início` ASC;
