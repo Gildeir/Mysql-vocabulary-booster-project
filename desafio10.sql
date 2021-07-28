d70a28ad36867b4aca56cdf77878d93159c779ea
Gildeir <gildeirlopes@gmail.com>
2021-07-28 19:32:19 -0300

    req 9

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT 
+product.ProductName AS `Produto`,
+MIN(orderDetail.Quantity) `Mínima`,
+MAX(orderDetail.Quantity) `Máxima` ,
+ROUND(AVG(orderDetail.Quantity), 2) `Média`
+FROM w3schools.products AS product
+INNER JOIN w3schools.order_details AS orderDetail
+ON product.ProductID = orderDetail.ProductID
+GROUP BY `Produto`
+HAVING `Média` > 20
+ORDER BY `Média` ASC, `Produto` ASC
