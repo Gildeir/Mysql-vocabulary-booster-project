67e8f42521ed1581f07bddae6538e37dede9481b
Gildeir <gildeirlopes@gmail.com>
2021-07-28 15:01:20 -0300

    corrigindo code climate

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT
 COUNTRY_NAME AS País,
-  IF (REGION_ID = 1, 'incluido', 'não incluido') AS 'Status Inclusão'
+  IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries
 ORDER BY País;
