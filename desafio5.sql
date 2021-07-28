f9da90a17c7ed161357c9c7bc795ffbc75e04c21
Gildeir <gildeirlopes@gmail.com>
2021-07-28 04:02:15 -0300

    Corrigindo crase

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,7 @@
-SELECT t1.JOB_TITLE AS Cargo,
-(MAX_SALARY - MIN_SALARY) 'Varicação Salarial',
-ROUND(MIN_SALARY / 12, 2) 'Média mínima mensal ',
-ROUND(MAX_SALARY / 12, 2) 'Média máxima mensal'
-FROM hr.jobs AS t1
-INNER JOIN hr.employees AS t2
-ON t1.JOB_ID = t2.JOB_ID
-GROUP BY Cargo
-ORDER BY 'Varicação Salarial', Cargo;
+SELECT jobs.JOB_TITLE AS `Cargo`,
+(jobs.MAX_SALARY - jobs.MIN_SALARY) `Variação Salarial`,
+ROUND(jobs.MIN_SALARY / 12, 2) `Média mínima mensal`,
+ROUND(jobs.MAX_SALARY / 12, 2) `Média máxima mensal`
+FROM hr.jobs
+ORDER BY `Variação Salarial` ASC;
+
