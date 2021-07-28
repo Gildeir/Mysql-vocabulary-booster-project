21fa0bcfe23a3ca5c25eb0d017f7cdad710e9bb4
Gildeir <gildeirlopes@gmail.com>
2021-07-28 15:45:27 -0300

    req8

--- desafio8.sql
+++ desafio8.sql
@@ -4,7 +4,7 @@ t1.ShipperName As `Empresa que fez o envio`,
 t2.OrderDate AS `Data do pedido`
 FROM w3schools.shippers AS t1
 INNER JOIN w3schools.orders AS t2
-ON t2.ShipperID = t2.ShipperID
+ON t2.ShipperID = t1.ShipperID
 INNER JOIN w3schools.customers AS t3
 ON t2.CustomerID = t3.CustomerID
 WHERE t1.ShipperID = 1 OR t1.ShipperID = 2
