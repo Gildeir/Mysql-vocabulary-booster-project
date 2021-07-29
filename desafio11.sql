0cc6377d9cdceb97d764bef247f0dfd3eabb77d9
Gildeir <gildeirlopes@gmail.com>
2021-07-28 23:42:52 -0300

    corrigindo lógica req11

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT 
+t1.ContactName `Nome`,
+t1.Country `País`,
+COUNT(t1.Country) `Número de compatriotas`
+FROM w3schools.customers AS t1, w3schools.customers AS t2
+WHERE t1.Country = t2.Country AND t1.CustomerID <> t2.CustomerID
+GROUP BY t1.CustomerID
+ORDER BY `Nome` ASC;
